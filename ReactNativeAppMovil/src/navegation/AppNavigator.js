6f60a36d4cb3d18ea84f175fee28556a552a4578
Farrauto <jvfarrauto06@gmail.com>
2025-04-11 22:52:35 -0400

    deleted unnecessary files

--- ReactNativeAppMovil/src/navegation/AppNavigator.js
+++ ReactNativeAppMovil/src/navegation/AppNavigator.js
@@ -4,10 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import RecipeListScreen from "../screens/RecipeListScreen";
-import HomeScreen from "../screens/HomeScreen";
 import RecipeDetailScreen from "../screens/RecipiesDetails";
-import apiScreen from "../screens/apiScreen";
-import RecipieDetailsScreen from "../screens/RecipiesDetails";
 
 const Stack = createStackNavigator();
 
@@ -16,10 +13,7 @@ const AppNavigator = () => (
         <Stack.Navigator ScreenOptions={{headerShown: false}}>
             <Stack.Screen name="Let's Him Coock" component={WelcomeScreen} />
             <Stack.Screen name="RecipeList" component={RecipeListScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
-            <Stack.Screen name="apiScreen" component={apiScreen} />
-            <Stack.Screen name="RecepieDetails" component={RecipieDetailsScreen} />
+            <Stack.Screen name="RecepieDetails" component={RecipeDetailScreen} />
         </Stack.Navigator>
     </NavigationContainer>
 ); 
